262fab0c5e89f680aae4ebb336055fc11e75eda4
Najwan Caesar Firstiansyah <najwan12311@gmail.com>
2024-10-28 07:57:37 +0000

    2404023

---
+++ .freeCodeCamp/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Program to run my four other programs
+./questionnaire.sh
+./countdown.sh a
+./bingo.sh
+./fortune.sh
---
+++ five.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Program to run my four other programs
+./questionnaire.sh
+./countdown.sh 3
+./bingo.sh
+./fortune.sh
